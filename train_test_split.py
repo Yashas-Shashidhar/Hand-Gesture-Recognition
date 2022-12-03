e42fe8577be5bb52621e991d71d4dd02e6a4df02
Yashas-Shashidhar <119695949+Yashas-Shashidhar@users.noreply.github.com>
2022-12-02 23:43:30 -0600

    Update train_test_split.py

--- train_test_split.py
+++ train_test_split.py
@@ -1,7 +1,7 @@
 import splitfolders
 
-splitfolders.ratio("/Users/shreenidhir/Documents/Machine learning/project_18nov/rps_data/", # The location of dataset
-                   output="/Users/shreenidhir/Documents/Machine learning/project_18nov/rps_train_test_val_split", # The output location
+splitfolders.ratio("rps_data", # The location of dataset
+                   output="rps_train_test_val_split", # The output location
                    seed=42, # The number of seed
                    ratio=(.7, .2, .1), # The ratio of splited dataset
                    group_prefix=None, # If your dataset contains more than one file like ".jpg", ".pdf", etc
